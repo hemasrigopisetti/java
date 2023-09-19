05d4fd4369c7c34457c165455d93723808938601
hemasrigopisetti <143474028+hemasrigopisetti@users.noreply.github.com>
2023-09-18 21:06:08 -0700

    Add files via upload

---
+++ DefaultDemo.java
@@ -0,0 +1,11 @@
+class Sample {
+    void display() {
+        System.out.println("Hello world");
+    }
+}
+class DefaultDemo {
+    public static void main(String args[]) {
+        Sample s=new Sample();
+        s.display();
+    }
+}
