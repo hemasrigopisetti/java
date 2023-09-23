af3cfe57603eb6dc95313b9b86c963204c7ba02d
hemasrigopisetti <143474028+hemasrigopisetti@users.noreply.github.com>
2023-09-23 15:20:53 +0530

    Add files via upload

---
+++ InheritanceDemo.java
@@ -0,0 +1,27 @@
+import java.io.*;
+class Shape{}
+class Rectangle extends Shape{}
+class Square extends Shape{}
+class Rhombus extends Shape{}
+class Animals
+{
+    String name;
+    public void eat()
+    {
+        System.out.println("I can eat");
+    }
+}
+class JerryMouse extends Animals{
+    public void display()
+    {
+        System.out.println("My name is "+name);
+    }
+}
+public class InheritanceDemo {
+    public static void main(String args[]) {
+        JerryMouse jm=new JerryMouse();
+        jm.name="jerry_the mouse";
+        jm.display();
+        jm.eat();
+    }
+}
