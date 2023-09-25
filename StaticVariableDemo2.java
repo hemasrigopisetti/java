d6b50cb466f6f3489cbf3fcd0fe90489a5c2ff65
hemasrigopisetti <143474028+hemasrigopisetti@users.noreply.github.com>
2023-09-25 12:09:02 +0530

    Add files via upload

---
+++ StaticVariableDemo2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class CounterDemo
+{
+    static int count=0;
+    CounterDemo()
+    {
+        count++;
+        System.out.println("Count= "+count);
+    }
+}
+public class StaticVariableDemo2
+{
+    public static void main(String args[])
+    {
+        CounterDemo cd=new CounterDemo();
+        CounterDemo cd1=new CounterDemo();
+        CounterDemo cd2=new CounterDemo();
+    }
+}
