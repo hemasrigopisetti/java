417e2811fbf6c93978adc48ad4ef2b28669cdf08
hemasrigopisetti <143474028+hemasrigopisetti@users.noreply.github.com>
2023-09-25 11:40:33 +0530

    Add files via upload

---
+++ ThisMethodDemo.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class A
+{
+    void m()
+    {
+        System.out.println("Inside method m");
+    }
+    void n()
+    {
+        System.out.println("Inside method n");
+        this.m();
+    }
+}
+public class ThisMethodDemo
+{
+    public static void main(String args[])
+    {
+        A a=new A();
+        a.n();
+    }
+}
