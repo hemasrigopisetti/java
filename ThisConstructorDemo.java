c0f2849b2a78cd726d9334fa18893b78c19aacf1
hemasrigopisetti <143474028+hemasrigopisetti@users.noreply.github.com>
2023-09-25 11:46:24 +0530

    Add files via upload

---
+++ ThisConstructorDemo.java
@@ -0,0 +1,20 @@
+import java.io.*;
+class A
+{
+    A()
+    {
+        System.out.println("Inside default constructor");
+    }
+    A(int x)
+    {
+        this();
+        System.out.println("x value is "+ x);
+    }
+}
+public class ThisConstructorDemo
+{
+    public static void main(String args[])
+    {
+        A a=new A(4225);
+    }
+}
